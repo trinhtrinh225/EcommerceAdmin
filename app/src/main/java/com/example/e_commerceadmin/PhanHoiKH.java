641fc8264d710969b226e8b8efcbf06e187641a7
trinhtrinh225 <thuctrinh.22598@gmail.com>
2020-12-18 10:36:19 +0700

    UPDATE IMAGE CATEGORY

--- app/src/main/java/com/example/e_commerceadmin/PhanHoiKH.java
+++ app/src/main/java/com/example/e_commerceadmin/PhanHoiKH.java
@@ -4,6 +4,7 @@ package com.example.e_commerceadmin;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PhanHoiKH extends AppCompatActivity {
@@ -12,6 +13,10 @@ public class PhanHoiKH extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phan_hoi_k_h);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Phản hồi khách hàng");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
