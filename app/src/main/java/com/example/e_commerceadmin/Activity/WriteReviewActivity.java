4df8079f29449c3be416694fd368b43539822e93
DELL <bangbui947@gmail.com>
2020-12-24 07:33:48 +0700

    danh gia san pham

---
+++ app/src/main/java/com/example/e_commerceadmin/Activity/WriteReviewActivity.java
@@ -0,0 +1,17 @@
+package com.example.e_commerceadmin.Activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.e_commerceadmin.R;
+
+public class WriteReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.write_review_layout);
+    }
+}
